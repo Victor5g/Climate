82b194ab159963a95b88fdf213469ce7b989c334
Victor Morramidy <victor.sales@beegol.com>
2022-04-21 18:57:34 -0300

    Feat: Horizontal Scroll

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,4 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
+import { Image, ScrollView, View } from 'react-native'
 
 import { 
   Wrapper, 
@@ -6,28 +7,95 @@ import {
   Gradient,
   ContentTitle,
   ContentIlustration,
-  TitleTemp
+  TitleTemp,
+  TextMoisture,
+  TextBold,
+  TextDay,
+  TextDate,
+  TextAddres,
+  ButtonUpdate,
+  WrapperScroll,
+  ItemContentScroll,
+  ContentBody
 } from './styles';
 
+//icon
+import { Entypo } from '@expo/vector-icons'; 
+import { MaterialIcons } from '@expo/vector-icons';
+
+//themes
 import themes from '../../themes';
+
+//images
+import Rain from '../../assets/illustrations/Rain.png'
+
 
 export default function Home() {
 
   return (
     <Gradient theme={themes.cloudyTheme.background}>
       <Wrapper>
+       <ScrollView 
+       scrollEnabled={true} 
+       showsVerticalScrollIndicator={false} 
+       style={{width:'100%', height:'100%'}}>
+         <ContentBody>
+            <ContentTitle>
+                <Title>Dia chuvoso e frio</Title>
+            </ContentTitle>
 
-      <ContentTitle>
-        <Title>Dia chuvoso e frio</Title>
-      </ContentTitle>
+            <ContentIlustration>
+                <Image style={{width:"80%", height:"100%", position:'absolute', right:-16, bottom:40}} source={Rain} />
+                <TitleTemp>19&deg;</TitleTemp>
+            </ContentIlustration>
 
-      <ContentIlustration>
-        <TitleTemp>19&deg;</TitleTemp>
-      </ContentIlustration>
-      
+              <TextMoisture>
+                  <Entypo name="drop" size={24} color="white"/>{" "}
+                  65%
+              </TextMoisture>
 
+              <TextDay>
+                <TextBold>Segunda feira{" "}</TextBold>
+                <TextDate>10 de agosto de 2022</TextDate> 
+              </TextDay>
+
+              <TextAddres>
+                R. Mario Pereira, 42, Pres. Dutra - MA, 65760-000, 
+              </TextAddres>
+       
+              <WrapperScroll>
+                <ScrollView 
+                horizontal={true}
+                showsHorizontalScrollIndicator={false}
+                alwaysBounceHorizontal={true}
+                  >
+                    <ItemContentScroll>
+
+                    </ItemContentScroll>
+                    <ItemContentScroll>
+
+                    </ItemContentScroll>
+                    <ItemContentScroll>
+
+                    </ItemContentScroll>
+                    <ItemContentScroll>
+
+                    </ItemContentScroll>
+                    <ItemContentScroll>
+
+                    </ItemContentScroll>
+
+                  </ScrollView>
+           
+              </WrapperScroll>
+          </ContentBody>
+       </ScrollView>
+
+       <ButtonUpdate onPress={()=>{console.log("Update")}}>
+          <MaterialIcons name="my-location" size={30} color="white"/>
+       </ButtonUpdate>
     </Wrapper>
-    {/* <StatusBar style='auto'/> */}
+    <StatusBar style='auto'/>
     </Gradient>
   );
 }
--- src/screens/home/styles.tsx
+++ src/screens/home/styles.tsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components/native';
 import { LinearGradient } from 'expo-linear-gradient';
-import { SafeAreaView } from 'react-native'
+import { SafeAreaView, Platform, Dimensions } from 'react-native'
 
 type GradientTypes = {
   theme:Array<string>;
@@ -16,11 +16,16 @@ export const Gradient = ({theme, children}:GradientTypes) =>{
   </LinearGradient>
   )
 }
+//height: ${Dimensions.get('window').height+'px'};
+export const ContentBody = styled.View`
+width:${Dimensions.get('window').width+'px'};
+height:910px;
+padding:${Platform.OS === 'ios'? `13px 16px 13px 16px` : `30px 16px 13px 16px`};
+`;
 
 export const Wrapper = styled.View`
 width:100%;
 height: 100%;
-padding:13px 16px 13px 16px;
 align-items:flex-start;
 justify-content:flex-start;
 flex-direction:column;
@@ -38,7 +43,8 @@ font-size:28px;
 
 export const ContentIlustration = styled.View`
 width:100%;
-height:40%;
+height:45%;
+top:16px;
 align-items:flex-start;
 justify-content:flex-end;
 
@@ -46,6 +52,69 @@ justify-content:flex-end;
 
 export const TitleTemp = styled.Text`
 color:#FFF;
-font-size:120px;
+font-size:${Platform.OS === 'ios'? `120px` : `100px`};
 font-weight:100;
+`;
+
+
+export const TextBold = styled.Text`
+font-weight:700;
+`;
+
+export const TextMoisture = styled.Text`
+color:#FFF;
+top:8px;
+font-size:22px;
+font-weight:300;
+`;
+
+export const TextDay = styled.Text`
+color:#FFF;
+top:20px;
+font-size:22px;
+font-weight:100;
+`;
+
+export const TextDate = styled.Text`
+color:#FFF;
+top:20px;
+font-size:18px;
+font-weight:100;
+`;
+
+export const TextAddres = styled.Text`
+ top:30px;
+ color:#FFF;
+ font-size:18px;
+`;
+
+export const WrapperScroll = styled.View`
+ top:40px;
+ width:${Dimensions.get('window').width+'px'};
+ left:-16px;
+`;
+
+
+export const ItemContentScroll = styled.View`
+width:120px;
+height:120px;
+margin-left:5px ;
+margin-right:5px ;
+justify-content:space-between ;
+background-color:#A9C5CE ;
+opacity:0.5 ;
+border-radius:12px;
+`;
+
+export const ButtonUpdate = styled.TouchableOpacity`
+ position:absolute ;
+ align-items:center;
+ justify-content:center;
+ width:70px;
+ height:70px;
+ border-radius:100px ;
+ right: 20px;
+ bottom:40px;
+ background-color:#1F5467;
+ box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
 `;
